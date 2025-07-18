e733d65b7cf2ee2affc3aa51c3a674d906cdd360
Alex Toumazis <alex.toumazis@gmail.com>
2025-07-18 15:02:15 +0300

    typo

--- src/helpers.py
+++ src/helpers.py
@@ -2,7 +2,7 @@ import os
 from datetime import date
 
 def get_root_folder_for_day(day: date):
-    return f"/app/summaries{day.strftime('%Y-%m-%d')}/"
+    return f"/app/summaries/{day.strftime('%Y-%m-%d')}/"
 
 def get_media_folder_for_day(day: date):
     return f"/app/summaries/{day.strftime('%Y-%m-%d')}/media/"
